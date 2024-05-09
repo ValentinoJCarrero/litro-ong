4fe8009529d090a0c714f2c6807ec7d76d696409
Miguel-Angel-tapia <tapiamiguelybr@gmail.com>
2024-05-09 11:40:05 -0300

    implementacion documentacion con swagger, Configuracion DB postgres

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
