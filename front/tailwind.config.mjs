12107e2b679fa5fa76add2d9727c3998d4e01538
Julian Magallanes <julianmagallanes1994@gmail.com>
2024-05-10 11:37:03 -0300

    footer y nadvar

--- front/tailwind.config.mjs
+++ front/tailwind.config.mjs
@@ -20,7 +20,11 @@ export default {
 			boxShadow: {
 				'3xl': '0px 4px 4px 0px rgba(0, 0, 0, 0.25);',
 				'4xl': '0px 10px 40px 0px rgba(0, 0, 0, 0.40);'
-			},		
+			},
+			backgroundImage: {
+				'nalLink': "linear-gradient(180deg, rgba(242, 242, 242, 0.40) 0%, rgba(242, 242, 242, 0.00) 100%);",
+				'card': "linear-gradient(90deg, #FFF 37.57%, rgba(255, 255, 255, 0.81) 50.79%, rgba(255, 255, 255, 0.66) 62.7%, rgba(255, 255, 255, 0.00) 73.15%);"
+			  }	
 		},
 	},
 	plugins: [],
