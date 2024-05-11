b328ad2b0a418e2bb1a7f69569c3ddacb0701bbe
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-05-11 15:55:14 +0200

    Puse una imagen simulando el carrucel, puse estilos basicos y he creado archivos .md para hacer las tarjetas de ser socio,sponsor o voluntario. Tengo muchos archivos comentados. No es oficial para subir a dev si bien funciona. Pero necesito limpiar codigo.

---
+++ front/src/components/fromHome/CarruselFromHome/CarruselFromHome.jsx
@@ -0,0 +1,30 @@
+// // 'use client';
+// // import { Carousel } from "flowbite-react";
+// import COPIAimgPortada from "../../assets/COPIAimgPortada.jpg";
+// import ButtonCTA from "../../components/Buttons/ButtonCTA.astro";
+
+// export function CarruselFromHome() {
+//   return (
+//     <div className="relative">
+//       <div
+//         className="absolute inset-0"
+//         style={{
+//           backgroundImage: `linear-gradient(to right, rgba(255, 255, 255,0.99) 35%, transparent 100%)`,
+//         }}
+//       ></div>
+//       <img
+//         src={COPIAimgPortada.src}
+//         alt="laimagen"
+//         className="w-full object-cover bg-cover bg-center h-full"
+//         style={{ backgroundImage: `url(${COPIAimgPortada.src})` }}
+//       />
+//       <div className="absolute inset-0 top-24 left-44 flex flex-col">
+//         <h1 className="text-6xl font-bold mb-3">Nuestra Misión</h1>
+//         <h2 className="text-4xl w-3/5 font-medium">Estamos permanentemente activos para reducir las desigualdades socioeconómicas existentes en la sociedad.</h2>
+//       </div>
+//     </div>
+
+//   );
+// }
+
+// export default CarruselFromHome;
