98f8ec71d81a38617601f55900abfd5e929007b8
Miguel-Angel-tapia <tapiamiguelybr@gmail.com>
2024-05-11 13:12:46 -0300

    Creacion de Modulos benefit - news - sponsor, con manejo de errores generales

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+//import { TypeOrmModule } from '@nestjs/typeorm';
+//import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NewsModule } from './modules/news/news.module';
 import { SponsorModule } from './modules/sponsor/sponsor.module';
 import { BenefitModule } from './modules/benefit/benefit.module';
-import dbConfig from './config/dbConfig';
+//import dbConfig from './config/dbConfig';
 
 @Module({
   imports: [
