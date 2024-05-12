2f68e9047efd9e8099b09954b086e58e1bb417f5
Miguel-Angel-tapia <tapiamiguelybr@gmail.com>
2024-05-11 21:12:17 -0300

    validaciones completas, entidad y dto modificado - NEWS

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,24 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { TypeOrmModule } from '@nestjs/typeorm';
-//import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NewsModule } from './modules/news/news.module';
 import { SponsorModule } from './modules/sponsor/sponsor.module';
 import { BenefitModule } from './modules/benefit/benefit.module';
-//import dbConfig from './config/dbConfig';
+import dbConfig from './config/dbConfig';
 
 @Module({
   imports: [
-    /* ConfigModule.forRoot({
+    ConfigModule.forRoot({
       isGlobal: true,
       load: [dbConfig],
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-    useFactory: (configService: ConfigService) =>
+      useFactory: (configService: ConfigService) =>
         configService.get('dbConfig'),
-    }), */
+    }),
     NewsModule,
     SponsorModule,
     BenefitModule,
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
-import { auth } from 'express-openid-connect';
-import { auth0Config } from './config/auth0Config';
+import { ValidationPipe } from '@nestjs/common';
+//import { auth } from 'express-openid-connect';
+//import { auth0Config } from './config/auth0Config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   setupSwagger(app);
 
-  app.use(auth(auth0Config));
+  // app.use(auth(auth0Config));
 
   await app.listen(3000);
 }
