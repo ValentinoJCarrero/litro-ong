c9652df31eda8c52886c63649fca8191e5de2956
Julian Magallanes <julianmagallanes1994@gmail.com>
2024-05-12 12:36:26 -0300

    vercel eliminado

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -2,16 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   
-  redirects: {
-    '/dasboardAdmin': '/'
-    },
   integrations: [tailwind(), react({
     experimentalReactChildren: true,
   })],
-  output: 'server',
-  adapter: vercel(),
 });
