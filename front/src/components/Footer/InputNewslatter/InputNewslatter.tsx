0a166a72472e549de81bade0e1d32d4d553dbd2a
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-05-14 12:03:43 +0200

    modifiqué estilos del footer

--- front/src/components/Footer/InputNewslatter/InputNewslatter.tsx
+++ front/src/components/Footer/InputNewslatter/InputNewslatter.tsx
@@ -1,8 +1,8 @@
 const InputNewslatter:React.FC = ():React.ReactElement=>{
     return(
-        <div  className="h-16 w-[30rem] text-lg">
-            <input type="text" className="h-full w-2/3 rounded-l-3xl border border-textTertiary text-textTertiary font-medium placeholder:font-light placeholder:text-textParagraph p-5 focus-visible:outline " placeholder="Escribe tu mail"/>
-            <button className="bg-textTertiary text-textPrimary h-full w-1/3 rounded-r-3xl hover:text-xl">Subscribir</button>
+        <div  className="h-16 w-[30rem] text-lg flex flex-row">
+            <input type="text" className="h-14 w-2/3 rounded-l-3xl border border-textTertiary text-textTertiary font-medium placeholder:font-light placeholder:text-textParagraph p-5 focus-visible:outline " placeholder="Escribe tu mail"/>
+            <button className="bg-textTertiary text-textPrimary h-14 w-1/3 rounded-r-3xl hover:text-xl transition-all">Subscribir</button>
         </div>
     )
 }
