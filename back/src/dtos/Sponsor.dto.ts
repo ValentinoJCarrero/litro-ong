ff645ed95af433b1ba344fe5cab4dabe93f3a3f5
Miguel-Angel-tapia <tapiamiguelybr@gmail.com>
2024-05-15 16:12:32 -0300

    modulo beneficios y patrocinadores

--- back/src/dtos/Sponsor.dto.ts
+++ back/src/dtos/Sponsor.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsNotEmpty,
-  IsString,
-  IsUrl,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SponsorDto {
   /**
