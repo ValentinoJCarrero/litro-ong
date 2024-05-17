601c7e207e9b64f5a0435e2619250ebf0553c0dd
Julian Magallanes <julianmagallanes1994@gmail.com>
2024-05-17 18:09:25 -0300

    prueba ruta

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -8,9 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  redirects: {
-    '/dasboardAdmin': '/'
-  },
+  ssr: false,
   integrations: [tailwind(), react({
     experimentalReactChildren: true
   }), auth()],
---
+++ front/src/components/Navbar/Menu/Menu.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+const {signOut } = await import("auth-astro/client")
+interface MenuProps {
+  children: React.ReactNode;
+}
+
+const Menu: React.FC<MenuProps> = ({ children}) => {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  return (
+    <div className="relative">
+      <button className="flex items-center" onClick={toggleDropdown}>
+        {children}
+      </button>
+      <ul
+        className={`absolute mt-8 right-[-40px] w-80 z-10  bg-white shadow-lg rounded-lg ${
+          dropdownVisible ? "block" : "hidden"
+        }`}
+      >
+        <li className="  ">
+          <a href="/dashboardAdmin/news" className="flex flex-col px-4 p-2 bg-secondary rounded-t-lg hover:bg-gray-100 hover:rounded-lg">
+            <h4 className="text-sm font-medium text-textSecondary ">Menú de Control y Perfil</h4>
+            <p  className="text-sm font-base text-textSecondary ">Aquí encontrarás diversas opciones que podrás investigar</p>
+          </a>
+        </li>
+        <li className="text-2xl p-4 hover:bg-gray-100 hover:rounded-b-lg">
+          <button onClick={() => {
+            signOut()
+            window.location.reload()
+          }} className="text-textTertiary text-sm font-medium">Cerrar sesión</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
