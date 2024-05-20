b6ac308ec417e3615242cf873927ea1fd1de89b6
Nico <nicolasaddamo1@gmail.com>
2024-05-19 21:51:33 -0300

    mailer last

--- back/src/config/mailer.config.ts
+++ back/src/config/mailer.config.ts
@@ -1,5 +1,8 @@
 import sgMail from '@sendgrid/mail';
 import { config as dotenvconfig } from 'dotenv';
-dotenvconfig({ path: '.env' });
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+dotenvconfig({ path: '.env' });
+
+sgMail.setApiKey(process.env.RESEND_API_KEY);
+
+export default sgMail;
