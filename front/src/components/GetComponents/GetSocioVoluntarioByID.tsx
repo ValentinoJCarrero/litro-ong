5d20dab8f95ff3ffe8c58c65c47706b34b74ec24
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-05-25 17:13:28 +0200

    perfil del usuarioyvolunteer terminado en el dash del mismo

--- front/src/components/GetComponents/GetSocioVoluntarioByID.tsx
+++ front/src/components/GetComponents/GetSocioVoluntarioByID.tsx
@@ -4,13 +4,14 @@ import { jwtDecode } from "jwt-decode";
 import EnConstruccion from "../EnConstruccion/EnConstruccion";
 import { getVolunteersByID } from "../../helpers/SocioVoluntario/getUserSocioVoluntarioByID";
 import { useEffect, useState } from "react";
+import avatarUser from "../../assets/avatarUser.jpg";
 
 interface infoUser {
   fullName: string;
   phone: string;
   email: string;
   fullAddress: string;
-  role: Array<any>;
+  role: any;
   // birthDate: string;
   // dni: string;
   // donation: Array<string>;
@@ -35,7 +36,7 @@ function GetVolunteerByID() {
   try {
     const decodedToken: any = jwtDecode(tokenFromCookies);
     idDecodificado = decodedToken.userPayload.sub;
-    console.log("ID decodificado", idDecodificado);
+    // console.log("ID decodificado", idDecodificado);
   } catch (error) {
     console.error("Error al decodificar token", error);
     return;
@@ -54,12 +55,53 @@ function GetVolunteerByID() {
     }
   }, [idDecodificado]);
 
+  console.log(infoUser?.role[0].role)
+
   return (
-    <div>
+    <div className="h-full flex flex-col justify-between items-stretch ">
       {infoUser ? (
-        <div>
-          <h1>Información del socio voluntario</h1>
-          <p>Nombre: {infoUser?.fullName}</p>
+        <div className=" flex flex-col items-center gap-5 h-full w-full font-medium ">
+          <div className=" h-28 w-28 my-5">
+            <img src={avatarUser.src} alt="Avatar" className="rounded-3xl" />
+          </div>
+          <div className="flex flex-col w-3/4 gap-4 text-sm  ">
+            <hr />
+            <div className="flex flex-row  gap-12">
+              <span  className="   text-textParagraph w-1/3">
+               Nombre y apellido
+              </span>
+              <p className=" w-1/2">{infoUser?.fullName}</p>
+            </div>
+            <hr />
+            <div className="flex flex-row  gap-12 ">
+              <span  className="   text-textParagraph w-1/3">
+                Teléfono
+              </span>
+              <p className=" w-1/2">{infoUser?.phone}</p>
+            </div>
+            <hr />
+            <div className="flex flex-row  gap-12 ">
+              <span  className="   text-textParagraph w-1/3">
+                Email
+              </span>
+              <p className=" w-1/2">{infoUser?.email}</p>
+            </div>
+            <hr />
+            <div className="flex flex-row  gap-12 ">
+              <span  className="   text-textParagraph w-1/3">
+                Domicilio
+              </span>
+              <p className=" w-1/2">{infoUser?.fullAddress}</p>
+            </div>
+            <hr />
+            <div className="flex flex-row  gap-12 ">
+              <span  className="   text-textParagraph w-1/3">
+                Estado
+              </span>
+              <p className=" w-1/2">{infoUser?.role[0].role}</p>
+            </div>
+            <hr />
+          </div>
         </div>
       ) : (
         <div>
