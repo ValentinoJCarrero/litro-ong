9bbb0265122ebe2effa473d471ec57b690341321
Nico <nicolasaddamo1@gmail.com>
2024-05-27 07:26:21 -0300

    prev git pull

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -2,8 +2,11 @@ import { Controller, Get, OnModuleInit, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
-export class AppController  {
+export class AppController implements OnModuleInit {
   constructor(private readonly appService: AppService) {}
+  onModuleInit() {
+    this.seeder();
+  }
   @Get('seeder')
   seeder(): Promise<string> {
     return this.appService.seeder();
