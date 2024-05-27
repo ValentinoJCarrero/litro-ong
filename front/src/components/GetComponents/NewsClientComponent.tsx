4369e6b980f15dc3de82d2f0e788db4596ccfe85
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-05-27 12:16:45 +0200

    noticias de admin acomodada

--- front/src/components/GetComponents/NewsClientComponent.tsx
+++ front/src/components/GetComponents/NewsClientComponent.tsx
@@ -6,7 +6,7 @@ import SpinnersDelete from "../Spinners/SpinnersDelete";
 import { deleteNews } from "../../helpers/deleteNews";
 import SpinnersPrimary from "../Spinners/SpinnersPrimary";
 import NotFound from "../NotFound/NotFound";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 interface NewsItem {
   primaryImage: string;
   title: string;
@@ -18,9 +18,9 @@ interface NewsItem {
 }
 
 const NewsComponent = () => {
-  const [page, setPage] = useState (1)
-  const [message, setMessage] = useState ("")
-  const [totalPages, setTotalPages] = useState (3)
+  const [page, setPage] = useState(1);
+  const [message, setMessage] = useState("");
+  const [totalPages, setTotalPages] = useState(3);
   const [news, setNews] = useState<NewsItem[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isDeleting, setIsDeleting] = useState(false);
@@ -28,10 +28,10 @@ const NewsComponent = () => {
 
   useEffect(() => {
     const fetchNews = async (page: number) => {
-      const newsData = await getNews(3,page);
+      const newsData = await getNews(3, page);
       setNews(newsData.data);
       setMessage(newsData.message);
-      setTotalPages(Math.ceil(newsData.total/3));
+      setTotalPages(Math.ceil(newsData.total / 3));
       setIsLoading(false);
     };
     fetchNews(page);
@@ -47,22 +47,22 @@ const NewsComponent = () => {
       cancelButtonColor: "#EF4444",
       confirmButtonText: "Si, borrar!",
       cancelButtonText: "Cancelar",
-    }).then(async(result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
         setDeletingId(id);
-    setIsDeleting(true);
+        setIsDeleting(true);
 
-    await deleteNews(id);
+        await deleteNews(id);
 
-    setTimeout(() => {
-      setNews(news.filter((item) => item.id !== id));
-      setIsDeleting(false);
-      setDeletingId(null);
-    }, 1000);
+        setTimeout(() => {
+          setNews(news.filter((item) => item.id !== id));
+          setIsDeleting(false);
+          setDeletingId(null);
+        }, 1000);
         Swal.fire({
           title: "Eliminado!",
           text: "La noticia ha sido eliminada.",
-          icon: "success"
+          icon: "success",
         });
       }
     });
@@ -70,73 +70,91 @@ const NewsComponent = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-full flex-col">
+    <div className="flex flex-col flex-nowrap justify-between items-stretch p-4 h-full">
       {isLoading ? (
-        <div className="flex items-center justify-center">
-        <SpinnersPrimary />
+        <div className="flex items-center justify-center h-full">
+          <SpinnersPrimary />
         </div>
-      ) : message ==="No se encontraron noticias en esta pagina" ? (
-        <NotFound />
+      ) : message === "No se encontraron noticias en esta pagina" ? (
+        <div className="flex items-center justify-center h-full">
+          <NotFound />
+        </div>
       ) : (
-        <ul className=" w-full">
-          {news.map(({ primaryImage, title, subtitle, date, location, id }) => (
-            <>
-              <li
-                key={id}
-                className="flex flex-row flex-nowrap justify-between pr-10 items-center"
-              >
-                <a
-                  className="flex flex-row justify-between p-10 items-center text-sm w-full"
-                  id={`card${id}`}
-                  href={`/news/DinamicNew/${title}`}
-                >
-                  <div className="flex">
-                    <img
-                      src={primaryImage}
-                      alt={title}
-                      className="w-20 h-20 rounded-full object-cover mr-4"
-                    />
-                    <div>
-                      <h6 className="text-tertiary text-base font-semibold">
-                        {title}
-                      </h6>
-                      <p>{subtitle}</p>
-                      <p>{date}</p>
+        
+          <ul className=" w-full flex flex-col  ">
+            {news.map(
+              ({ primaryImage, title, subtitle, date, location, id }) => (
+                <div className="">
+                  <li
+                    key={id}
+                    className="flex flex-row my-1 flex-nowrap justify-between items-center"
+                  >
+                    <a
+                      className="flex flex-row justify-between items-center  text-sm w-full"
+                      id={`card${id}`}
+                      href={`/news/DinamicNew/${title}`}
+                    >
+                      <div className="flex">
+                        <img
+                          src={primaryImage}
+                          alt={title}
+                          className="w-20 h-20 rounded-full object-cover mr-4"
+                        />
+                        <div className="text-sm  text-textParagraph">
+                          <h6 className="text-tertiary text-base font-semibold">
+                            {title}
+                          </h6>
+                          <div className="text-sm  text-textParagraph">
+                            <p>{subtitle}</p>
+                            <p>{date}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                    <div className="flex flex-row justify-center gap-10">
+                      <div className="w-40 flex justify-center ">
+                        {isDeleting && deletingId === id ? (
+                          <SpinnersDelete />
+                        ) : (
+                          <ButtonWarningSmall
+                            title="Eliminar"
+                            idEvent={`delete-${id}`}
+                            onClick={() => onClic(id)}
+                          />
+                        )}
+                      </div>
+
+                      <img src={vectorIcon.src} alt="icono de vector" />
                     </div>
-                  </div>
-                  <div>
-                    <p>{location}</p>
-                  </div>
-                  <img src={vectorIcon.src} alt="icono de vector" />
-                </a>
-                <div className="w-40 flex justify-center">
-                  {isDeleting && deletingId === id ? (
-                    <SpinnersDelete />
-                  ) : (
-                    <ButtonWarningSmall
-                      title="Eliminar"
-                      idEvent={`delete-${id}`}
-                      onClick={() => onClic(id)}
-                    />
-                  )}
+                  </li>
+                  <hr />
                 </div>
-              </li>
-              <hr />
-              
-            </>
-          ))}
-          <div className="flex items-center justify-center flex-row w-full mt-8">
-              <div  className="rounded-lg w-12 h-12  flex items-center justify-center border border-backgroundGrey hover:bg-gray-300">
-                <button onClick={()=>(page > 1) && setPage(page - 1)} className="w-full h-full font-medium text-xl">{"<"}</button>
-              </div>
-                <p className=" font-base text-lg mx-4">{page}/{totalPages}</p>
-              <div  className="rounded-lg w-12 h-12  flex items-center justify-center border border-backgroundGrey hover:bg-gray-300">
-                <button onClick={()=>(page < totalPages) && setPage(page + 1)} className="w-full h-full font-medium text-xl">{">"}</button>
-              </div> 
-          </div>
-        </ul>
+              )
+            )}
+          </ul>
+        
       )}
-      
+          <div className="flex items-center justify-center flex-row w-full ">
+            <div className="rounded-lg w-8 h-8  flex items-center justify-center border border-backgroundGrey hover:bg-gray-300">
+              <button
+                onClick={() => page > 1 && setPage(page - 1)}
+                className="w-full h-full font-medium text-xl"
+              >
+                {"<"}
+              </button>
+            </div>
+            <p className=" font-base text-lg mx-4">
+              {page}/{totalPages}
+            </p>
+            <div className="rounded-lg w-8 h-8  flex items-center justify-center border border-backgroundGrey hover:bg-gray-300">
+              <button
+                onClick={() => page < totalPages && setPage(page + 1)}
+                className="w-full h-full font-medium text-xl"
+              >
+                {">"}
+              </button>
+            </div>
+          </div>
     </div>
   );
 };
