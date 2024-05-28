71810ef6041b99f9cbfac6b1761dd16d16ac28d6
Nico <nicolasaddamo1@gmail.com>
2024-05-28 13:58:50 -0300

    Cambio de formato mailer. prueba Crons

--- back/src/modules/mailer/mailer.controller.ts
+++ back/src/modules/mailer/mailer.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Put } from '@nestjs/common';
+import { Body, Controller, Post, Put,Get } from '@nestjs/common';
 import { MailerService } from './mailer.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
@@ -33,4 +33,24 @@ export class MailerController {
     return "La desuscripción fue exitosa";
   }
 
+    @Get('send-christmas-email')
+    async sendChristmasEmail() {
+        await this.mailerService.cronXMasMail();
+        return { message: 'Christmas email sent' };
+    }
+
+    @Get('send-newyear-email')
+    async sendNewYearEmail() {
+        await this.mailerService.cronNewyearMail();
+        return { message: 'New Year email sent' };
+    }
+
+    @Get('send-birthday-email')
+    async sendBirthdayEmail() {
+        await this.mailerService.birthdayGreetings();
+        return { message: 'Birthday email sent' };
+    }
+
+
 }
+
