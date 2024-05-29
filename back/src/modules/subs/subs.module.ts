e3d44ccbbaee798a17ea11e7ec8636241062f50d
ValentinoJC <80430338+ValentinoJCarrero@users.noreply.github.com>
2024-05-29 00:56:09 -0300

    Fix en SubsModule

--- back/src/modules/subs/subs.module.ts
+++ back/src/modules/subs/subs.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
 import { SubsController } from "./subs.controller";
 import { SubsService } from "./subs.service";
+import { PartnerService } from "../partner/partner.service";
+import { PartnerRepository } from "../partner/partner.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Partner } from "src/entities/Partner.entity";
+import { User } from "src/entities/User.entity";
+import { Role } from "src/entities/Role.entity";
+import { Card } from "src/entities/Card.entity";
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([Partner, User, Role, Card])],
     controllers: [SubsController],
-    providers: [SubsService]
+    providers: [SubsService, PartnerService, PartnerRepository]
 })
 export class SubsModule {}
