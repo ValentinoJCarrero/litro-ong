032226fd08722e68f64a087e90c6b3af38b46fcd
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-05-30 02:27:27 +0200

    terminada la de eventos

--- front/src/components/fromEvents/BannerIndividualEvent.tsx
+++ front/src/components/fromEvents/BannerIndividualEvent.tsx
@@ -16,15 +16,15 @@ const BannerIndividualEvent: React.FC<BannerIndividualEventProps> = ({
   profesor,
 }) => {
   return (
-    <div className="relative my-3 flex justify-end">
+    <div className="relative flex justify-end">
       <div
         className="absolute inset-0 z-10"
         style={{
           backgroundImage: `linear-gradient(to right, white 35%, transparent 100%)`,
         }}
       ></div>
-      <div className="absolute inset-0 top-24 left-36 w-2/5 z-20">
-        <div>
+      <div className="relative inset-0 h-full top-16 left-36 w-2/5 z-20 ">
+        <div className="flex  flex-col">
           <h1 className="text-5xl font-bold mb-3 leading-tight">{titulo}</h1>
           <h2 className="text-textTertiary text-xl font-normal tracking-widest">
             {fecha}
@@ -44,7 +44,7 @@ const BannerIndividualEvent: React.FC<BannerIndividualEventProps> = ({
       <img
         src={banner}
         alt="La imagen"
-        className="object-none bg-cover bg-center object-right  h-96 w-2/3   "
+        className="object-none bg-cover bg-center object-right my-2  h-96 w-2/3   "
         loading="lazy"
         decoding="async"
       />
